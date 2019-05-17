6357576fbfe3e20833750a326db55c7a5e231cdc
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-17 11:55:38 +0100

    Commented for clarity

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -25,6 +25,7 @@ const imgArray = Array.from(images).map(img => {
   return new Carousel(img);
 });
 
+// add click event handler to the right button
 rightButton.addEventListener("click", () => {
   if (currentImageIndex === 4) currentImageIndex = 0;
 
@@ -32,6 +33,7 @@ rightButton.addEventListener("click", () => {
   imgArray[currentImageIndex - 1].showImage();
 });
 
+// add click event handler to the left button
 leftButton.addEventListener("click", () => {
   if (currentImageIndex === 1) {
     currentImageIndex = 4;
@@ -42,12 +44,3 @@ leftButton.addEventListener("click", () => {
   currentImageIndex--;
   imgArray[currentImageIndex - 1].showImage();
 });
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
