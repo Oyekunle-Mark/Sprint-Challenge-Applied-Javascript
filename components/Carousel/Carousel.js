74b872613aeb34cca0c4977e4dca32d57f68b689
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-17 11:13:30 +0100

    Refactored the carousel.js file

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,41 +1,45 @@
 class Carousel {
   constructor(imgElement) {
     this.imgElement = imgElement;
-    this.currentImage = this.imgElement.dataset.img;
   }
 
   showImage() {
-    images.forEach(img => (img.style.display = "none"));
-    this.imgElement.style.display = "block";
+    images.forEach(img => img.classList.remove('show'));
+		this.imgElement.classList.add('show');
   }
 }
 
+// display the first image
+document.querySelector('img').classList.add('show');
+
+// get the carousel, images and buttons
 const carousel = document.querySelector(".carousel");
 const leftButton = carousel.querySelector(".left-button");
 const rightButton = carousel.querySelector(".right-button");
 const images = carousel.querySelectorAll("img");
-let buttonValue = 1;
+
+let currentImageIndex = 1;
 
 const imgArray = Array.from(images).map(img => {
   return new Carousel(img);
 });
 
 rightButton.addEventListener("click", () => {
-  if (buttonValue === 4) buttonValue = 0;
+  if (currentImageIndex === 4) currentImageIndex = 0;
 
-  buttonValue++;
-  imgArray[buttonValue - 1].showImage();
+  currentImageIndex++;
+  imgArray[currentImageIndex - 1].showImage();
 });
 
 leftButton.addEventListener("click", () => {
-  if (buttonValue === 1) {
-    buttonValue = 4;
-    imgArray[buttonValue - 1].showImage();
+  if (currentImageIndex === 1) {
+    currentImageIndex = 4;
+    imgArray[currentImageIndex - 1].showImage();
     return;
   }
 
-  buttonValue--;
-  imgArray[buttonValue - 1].showImage();
+  currentImageIndex--;
+  imgArray[currentImageIndex - 1].showImage();
 });
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
